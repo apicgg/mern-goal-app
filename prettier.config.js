88f85f8447e1c8e3a445db0a2276b4a62ce71b5c
Anurag Pramanik <p.anurag@outlook.com>
2022-11-22 19:03:22 +0530

    style(prettier): add prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  singleQuote: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  semi: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+}
+
+module.exports = options
