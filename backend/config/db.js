cd494a7597b7abb3e44b90afd0fa59fe99ef9c9d
Anurag Pramanik <p.anurag@outlook.com>
2022-11-30 16:33:52 +0530

    style(db.js): change the node console output style

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const connection = await mongoose.connect(process.env.MONGO_URI)
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log(
+      `🟢 MongoDB Connected: ${connection.connection.host}`.cyan.underline
+    )
   } catch (error) {
     console.log(error)
     process.exit(1)
