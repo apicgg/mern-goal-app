eb1de3262fbe8a714c3a9c7aa5dd1ea04bbc04fd
Anurag Pramanik <p.anurag@outlook.com>
2022-11-30 21:15:17 +0530

    feat(*): add user registration and login feature

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,51 @@
 const asyncHandler = require('express-async-handler')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-const User = require('../model/userModel')
+const User = require('../models/userModel')
 
 // @desc Register User
 // @route POST /api/users
 // @access Public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(201).json({ message: 'Register user' })
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all the fields')
+  }
+
+  // Check if the user exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create the user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @desc Authenticate a user
@@ -15,7 +53,23 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Login user' })
+  const { email, password } = req.body
+
+  // Check for user email
+  const user = await User.findOne({ email })
+
+  // Check for password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid credentials')
+  }
 })
 
 // @desc Get user data
@@ -25,5 +79,12 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get user data' })
 })
+
+// Generate jwt
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = { registerUser, loginUser, getMe }
