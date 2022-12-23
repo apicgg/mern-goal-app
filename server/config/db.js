891509eb994b8427117d796ef15f92b477794084
Anurag Pramanik <p.anurag@outlook.com>
2022-12-23 21:56:59 +0530

    build(dep): remove color npm package

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI)
+    const connection = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(
-      `🟢 MongoDB Connected: ${connection.connection.host}`.cyan.underline
-    )
+    console.log(`🟢 MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
-    console.log(error)
-    process.exit(1)
+    console.log(error);
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
